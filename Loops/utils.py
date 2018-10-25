8fd6fca520b03ac70ea2396218dd59345825c39c
JonBouw <J.Bouwknegt@tudeft.nl>
2018-10-25 14:37:49 +0200

    Loop script added to git repo

---
+++ Loops/utils.py
@@ -0,0 +1,4 @@
+# First make the function:
+def print_hello(): #defeition, give it a usefull name
+    """"This function provides me a helo""" # This you will ee when you call the function with shift+tab+tab
+    print("hello")
